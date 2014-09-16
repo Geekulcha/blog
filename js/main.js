9462102c8a981fc36476a5ff4ca1a7c09f61f644
Raoul DIFFOUO <diraulo@gmail.com>
2014-09-16 17:37:05 +0200

    Built gk-blog-jekyll from commit 8ff635e on branch develop

---
+++ js/main.js
@@ -0,0 +1,63 @@
+/*
+ * Template Name: Unify - Responsive Bootstrap Template
+ * Description: Business, Corporate, Portfolio and Blog Theme.
+ * Version: 1.4
+ * Author: @htmlstream
+ * Website: http://htmlstream.com
+ */
+
+// var App = function () {
+//
+//   function handleIEFixes() {
+//     //fix html5 placeholder attribute for ie7 & ie8
+//     if (jQuery.browser.msie && jQuery.browser.version.substr(0, 1) < 9) { // ie7&ie8
+//       jQuery('input[placeholder], textarea[placeholder]').each(function () {
+//         var input = jQuery(this);
+//
+//         jQuery(input).val(input.attr('placeholder'));
+//
+//         jQuery(input).focus(function () {
+//           if (input.val() == input.attr('placeholder')) {
+//             input.val('');
+//           }
+//         });
+//
+//         jQuery(input).blur(function () {
+//           if (input.val() == '' || input.val() == input.attr('placeholder')) {
+//             input.val(input.attr('placeholder'));
+//           }
+//         });
+//       });
+//     }
+//   }
+//
+//   function handleBootstrap() {
+//     /*Bootstrap Carousel*/
+//     jQuery('.carousel').carousel({
+//       interval: 15000,
+//       pause: 'hover'
+//     });
+//
+//     /*Tooltips*/
+//     jQuery('.tooltips').tooltip();
+//     jQuery('.tooltips-show').tooltip('show');
+//     jQuery('.tooltips-hide').tooltip('hide');
+//     jQuery('.tooltips-toggle').tooltip('toggle');
+//     jQuery('.tooltips-destroy').tooltip('destroy');
+//
+//     /*Popovers*/
+//     jQuery('.popovers').popover();
+//     jQuery('.popovers-show').popover('show');
+//     jQuery('.popovers-hide').popover('hide');
+//     jQuery('.popovers-toggle').popover('toggle');
+//     jQuery('.popovers-destroy').popover('destroy');
+//   }
+//
+//   return {
+//     init: function () {
+//       handleBootstrap();
+//       handleIEFixes();
+//     },
+//   };
+//
+// }();
